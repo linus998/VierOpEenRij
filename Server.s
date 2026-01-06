5ca6d8d4445b380cff62657ab2efbe9d0a7f4df7
Linus Beheydt <Linus.beheydt.psd@gmail.com>
2026-01-06 13:14:54 +0100

    use of stack

--- Server.s
+++ Server.s
@@ -12,19 +12,19 @@
 @ MAIN
 @ =============================================================================================
 main:
-	GPIOExport	pin24					@ config GPIO24
-	setSleepTime	#0, #100000000		@ 0.1 sec
-	doSleep								@ sleep
-	GPIODirectionOut  pin24				@ GPIO24 is output
+    GPIOExport    pin24                    @ config GPIO24
+    setSleepTime    #0, #100000000        @ 0.1 sec
+    doSleep                                @ sleep
+    GPIODirectionOut  pin24                @ GPIO24 is output
 
-	GPIOExport	pin23					@ config GPIO23
-	setSleepTime	#0, #100000000		@ 0.1 sec
-	doSleep								@ sleep
-	GPIODirectionIn  pin23				@ GPIO23 is input
+    GPIOExport    pin23                    @ config GPIO23
+    setSleepTime    #0, #100000000        @ 0.1 sec
+    doSleep                                @ sleep
+    GPIODirectionIn  pin23                @ GPIO23 is input
 
-	GPIOWrite	pin24, high				@ Tx high
-	setSleepTime	#0, #100000000		@ 0.1 sec
-	doSleep								@ sleep
+    GPIOWrite    pin24, high                @ Tx high
+    setSleepTime    #0, #100000000        @ 0.1 sec
+    doSleep                                @ sleep
 
     @ print bord
     @----------------------------------------------------------------------------------------------
@@ -33,25 +33,45 @@ print_bord:
     bl      printf
 
 print_line_speelveld:
-    ldr     R0, =bord_speel_lijn    
-    mov     R1, "S"
+    push    {r4, lr}        @ preserve callee-saved + align stack
+
+    ldr     r0, =bord_speel_lijn
+    mov     r1, #1          @ %d #1
+    mov     r2, #2          @ %d #2
+    mov     r3, #3          @ %d #3
+
+    sub     sp, sp, #16     @ space for 4 more ints (4 × 4 bytes)
+
+    mov     r4, #4
+    str     r4, [sp, #0]    @ %d #4
+    mov     r4, #5
+    str     r4, [sp, #4]    @ %d #5
+    mov     r4, #6
+    str     r4, [sp, #8]    @ %d #6
+    mov     r4, #7
+    str     r4, [sp, #12]   @ %d #7
+
     bl      printf
 
+    add     sp, sp, #16     @ clean up stack
+    pop     {r4, lr}
+    bx      lr
+
 exit:
-	mov		R0, #0			@ return code 0
-	mov		R7, #1			@ exit
-	svc		0				@ call Linux
+    mov        R0, #0            @ return code 0
+    mov        R7, #1            @ exit
+    svc        0                @ call Linux
 
 @ DATA
 @ =================================================================================================
 .data
-pin23:	.asciz	"23"
-pin24:	.asciz	"24"
-low:	.asciz	"0"
-high:	.asciz	"1"
-pin_val: .asciz	"?"
+pin23:    .asciz    "23"
+pin24:    .asciz    "24"
+low:    .asciz    "0"
+high:    .asciz    "1"
+pin_val: .asciz    "?"
 
 bord_top: .asciz "┌---------------------------┐\n"
-bord_speel_lijn: .asciz "| %s |\n"
+bord_speel_lijn: .asciz "| %d | %d | %d | %d | %d | %d | %d |\n"
 bord_bottom: .asciz "└---------------------------┘\n"
 input_from_user: .asciz "Welke lijn wil je invoegen: \n"
